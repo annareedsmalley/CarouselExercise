3f9e4e31ab88389c9bc901a04130b117478eaff2
annareedsmalley <asmalley@walmartlabs.com>
2015-10-20 08:33:43 -0700

    Made some fixes based on Facebook support - e.g. connected the segue from sign in to the tutorial from the actual sign in view controller vs. the sign in button.

--- signInViewController.swift
+++ signInViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class signInViewController: UIViewController {
-
+    
     var initialY: CGFloat!
     let offset: CGFloat = -50
     
-     let emptyFieldAlertController = UIAlertController(title: "Oops - forget something?", message: "Please enter your email address and password", preferredStyle: .Alert)
+    let emptyFieldAlertController = UIAlertController(title: "Oops - forget something?", message: "Please enter your email address and password", preferredStyle: .Alert)
     
-     let incorrectCredentialsAlertController = UIAlertController(title: "Oops - try again", message: "Incorrect email or password", preferredStyle: .Alert)
+    let incorrectCredentialsAlertController = UIAlertController(title: "Oops - try again", message: "Incorrect email or password", preferredStyle: .Alert)
     
     let OKAction = UIAlertAction(title: "OK", style: .Default) { (hide) in  // handle response here.}
         
         
-      
-    //optional code here
+        
+        //optional code here
+        
+    }
     
-}
     
-
     @IBOutlet weak var signInContent: UIView!
     
     @IBOutlet weak var signInScrollView: UIScrollView!
@@ -33,9 +33,9 @@ class signInViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
-   
+    
     @IBOutlet weak var bothSignInButtons: UIImageView!
-
+    
     @IBOutlet weak var realBothSignInButtons: UIView!
     
     @IBOutlet weak var signInButton: UIButton!
@@ -43,7 +43,7 @@ class signInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        
         
         signInScrollView.contentSize = CGSize(width: 320, height: 568)
         
@@ -52,7 +52,7 @@ class signInViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -65,31 +65,32 @@ class signInViewController: UIViewController {
         
         realBothSignInButtons.transform = CGAffineTransformMakeTranslation( 0, -160)
         
-       
+        
         
     }
     
     func keyboardWillHide(notification: NSNotification!) {
         
     }
- 
+    
     
     
     @IBAction func didPressSignIn(sender: AnyObject) {
         
-    
+        
         if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
             
             self.emptyFieldAlertController.addAction(self.OKAction)
+            
             presentViewController(emptyFieldAlertController, animated: true){
                 
-
-            
+                
+                
             }
             
             
         }
-      
+        
         //var email = emailTextField.text
         //var password = passwordTextField.text
         
@@ -97,38 +98,38 @@ class signInViewController: UIViewController {
             print ("incorrect something")
             
             self.incorrectCredentialsAlertController.addAction(self.OKAction)
+            
             presentViewController(incorrectCredentialsAlertController, animated: true){
                 
                 
                 
             }
-
+            
         }
-        
+            
         else {
             
-        print ("both correct")
-        
-        performSegueWithIdentifier("loginSegue", sender: nil)
+            
+            performSegueWithIdentifier("realLoginSegue", sender: nil)
         }
         //&& passwordTextField.text = "password"
     }
     
     
- 
+    
     
     
     //print ("oops")
-        //presentViewController(emptyFieldAlertController, animated: true)
+    //presentViewController(emptyFieldAlertController, animated: true)
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
-    }
+    
+}
